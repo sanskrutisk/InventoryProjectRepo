d505a823d1474097344206a41c00d92086390209
komaldharmale <komaldharmale2019@gmail.com>
2024-12-12 13:39:05 +0530

    pre register msg

--- InventoryApp/src/main/java/com/cjc/main/controller/ProductController.java
+++ InventoryApp/src/main/java/com/cjc/main/controller/ProductController.java
@@ -1,8 +1,15 @@
 package com.cjc.main.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProductController {
 
+	@RequestMapping("/")
+	public String preLog() {
+		
+		return "Register here";
+	}
+	
 }
